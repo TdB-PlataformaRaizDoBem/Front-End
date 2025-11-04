4896016acb2a73a92d7e68799bf2ef3b52bbb94b
Renan Paulino <dev.renanpaulino@gmail.com>
2025-11-04 20:07:00 -0300

    responsividade da nav interna da plataforma e pág de contato

--- js/asidebar.js
+++ js/asidebar.js
@@ -1,9 +1,20 @@
 const asidebar = document.getElementById('asidebar');
 const btnClose = document.getElementById('toggleAside');
+const open = document.getElementById('open-menu-mobile');
+const btnCloseMobile = document.getElementById('toggleAside-mobile');
 
 btnClose.addEventListener('click', () => {
   asidebar.classList.toggle('collapsed')
   document.body.classList.toggle('aside-collapsed');
+})
+
+open.addEventListener('click', () => {
+  document.body.classList.toggle('aside-collapsed');
+  asidebar.style.display="block"
+})
+
+btnCloseMobile.addEventListener('click', () => {
+  asidebar.style.display="none"
 })
 
 const toggleTheme = document.querySelector('.toggle-theme');
